ebea12cb3bb0ce6ae04c8aa3689b668bb419e080
Marcelo Zabani <mzabani@gmail.com>
2022-11-12 15:44:06 -0300

    Make instances generic in the Monad type, document test

--- src/AesonStream.hs
+++ src/AesonStream.hs
@@ -25,55 +25,55 @@ import           GHC.TypeLits                   ( KnownSymbol
                                                 )
 
 
-class ToJSONStream a where
-    toJSONStream :: a -> Stream (Of ByteString) IO ()
-    default toJSONStream :: (Generic a, JG (Rep a)) => a -> Stream (Of ByteString) IO ()
+class ToJSONStream m a where
+    toJSONStream :: a -> Stream (Of ByteString) m ()
+    default toJSONStream :: (Monad m, Generic a, JG m (Rep a)) => a -> Stream (Of ByteString) m ()
     toJSONStream = genericToJSONStream
 
 genericToJSONStream
-    :: (Generic a, JG (Rep a)) => a -> Stream (Of ByteString) IO ()
+    :: (Monad m, Generic a, JG m (Rep a)) => a -> Stream (Of ByteString) m ()
 genericToJSONStream v = jg (from v)
 
-class JG f where
-    jg :: f x -> Stream (Of ByteString) IO ()
+class JG m f where
+    jg :: f x -> Stream (Of ByteString) m ()
 
-instance ToJSONStream a => JG (K1 i a) where
+instance ToJSONStream m a => JG m (K1 i a) where
     jg (K1 v) = toJSONStream v
 
-instance (JG a, JG b) => JG (a :*: b) where
+instance (Monad m, JG m a, JG m b) => JG m (a :*: b) where
     jg (a :*: b) = jg a >> S.yield "," >> jg b
 
-instance (ToJSONStream a, KnownSymbol fieldName) => JG (S1 ('MetaSel ('Just fieldName) x y z) (K1 i a)) where
+instance (Monad m, ToJSONStream m a, KnownSymbol fieldName) => JG m (S1 ('MetaSel ('Just fieldName) x y z) (K1 i a)) where
     jg (M1 v) =
         S.yield "\""
             >> S.yield (encodeUtf8 $ LT.pack $ symbolVal (Proxy @fieldName))
             >> S.yield "\":"
             >> toJSONStream (unK1 v)
 
-instance (JG a) => JG (D1 x (C1 ('MetaCons y z 'True) a)) where
+instance (Monad m, JG m a) => JG m (D1 x (C1 ('MetaCons y z 'True) a)) where
     jg (M1 (M1 fields)) = S.yield "{" >> jg fields >> S.yield "}"
 
--- instance (ToJSONStream a, ToJSONStream b) => JG (a :*: b) where
+-- instance (ToJSONStream m a, ToJSONStream b) => JG (a :*: b) where
 --     jg (a :*: b) = \x y -> toJSONStream x >> S.yield "," >> toJSONStream y
 
 newtype UseToJSONInstance a = UseToJSONInstance a
 
-instance ToJSON a => ToJSONStream (UseToJSONInstance a) where
+instance (Monad m, ToJSON a) => ToJSONStream m (UseToJSONInstance a) where
     toJSONStream (UseToJSONInstance v) = S.yield $ encode v
 
-deriving via (UseToJSONInstance Int) instance (ToJSONStream Int)
-deriving via (UseToJSONInstance Float) instance (ToJSONStream Float)
-deriving via (UseToJSONInstance Double) instance (ToJSONStream Double)
-deriving via (UseToJSONInstance Bool) instance (ToJSONStream Bool)
-deriving via (UseToJSONInstance LT.Text) instance (ToJSONStream LT.Text)
-deriving via (UseToJSONInstance T.Text) instance (ToJSONStream T.Text)
+deriving via (UseToJSONInstance Int) instance (Monad m => ToJSONStream m Int)
+deriving via (UseToJSONInstance Float) instance (Monad m => ToJSONStream m Float)
+deriving via (UseToJSONInstance Double) instance (Monad m => ToJSONStream m Double)
+deriving via (UseToJSONInstance Bool) instance (Monad m => ToJSONStream m Bool)
+deriving via (UseToJSONInstance LT.Text) instance (Monad m => ToJSONStream m LT.Text)
+deriving via (UseToJSONInstance T.Text) instance (Monad m => ToJSONStream m T.Text)
 
 -- | This instance emits JSON arrays from a stream of jsonifiable values.
-instance ToJSONStream a => ToJSONStream (Stream (Of a) IO ()) where
+instance (Monad m, ToJSONStream m a) => ToJSONStream m (Stream (Of a) m ()) where
     toJSONStream v =
         S.yield "[" >> S.intersperse "," (S.for v toJSONStream) >> S.yield "]"
 
-instance ToJSONStream a => ToJSONStream [a] where
+instance (Monad m, ToJSONStream m a) => ToJSONStream m [a] where
     -- Does this lazily traverse the list?
     -- TODO: Take advantage of the other instance?
     toJSONStream v =
@@ -81,17 +81,17 @@ instance ToJSONStream a => ToJSONStream [a] where
             >> S.intersperse "," (S.for (S.each v) toJSONStream)
             >> S.yield "]"
 
-instance ToJSONStream a => ToJSONStream (Maybe a) where
+instance (Monad m, ToJSONStream m a) => ToJSONStream m (Maybe a) where
     toJSONStream Nothing  = S.yield "null"
     toJSONStream (Just v) = toJSONStream v
 
-data TestObject = TestObject
+data TestObject m = TestObject
     { age           :: Int
-    , streamArray   :: Stream (Of Int) IO ()
+    , streamArray   :: Stream (Of Int) m ()
     , field         :: [Int]
     , thirdField    :: Maybe Bool
     , lazyTextField :: LT.Text
-    , otherObj      :: Maybe TestObject
+    , otherObj      :: Maybe (TestObject m)
     }
     deriving stock Generic
-    deriving anyclass ToJSONStream
+    deriving anyclass (ToJSONStream m)
--- test/MemoryUsageSpec.hs
+++ test/MemoryUsageSpec.hs
@@ -10,19 +10,22 @@ import qualified Streaming.Prelude             as S
 import Test.Hspec (Spec, it, shouldSatisfy)
 
 
-data VeryLargeObj = VeryLargeObj {
-    field1 :: Stream (Of Text) IO (),
-    field2 :: Stream (Of Text) IO ()
+data VeryLargeObj m = VeryLargeObj {
+    field1 :: Stream (Of Text) m (),
+    field2 :: Stream (Of Text) m ()
     }
     deriving stock Generic
-    deriving anyclass ToJSONStream
+    deriving anyclass (ToJSONStream m)
 
 spec :: Spec
 spec = do
     it "Stream more than fits in memory" $ do
-        -- This uses more memory than my machine or free CI gives us, so should be
-        -- sufficient to test.
-        let numChars = 1024 * 1024 * 1000
+        -- 2^30 =~ 1GB chars 'A' streamed, plus a few characters that form
+        -- a JSON object `{ "field1": "AAA..", "field2:" "AAA..." }`
+        -- means more than `numChars` characters streamed in total.
+        -- This test only works if we limit GHC's runtime memory usage somehow, though.
+        -- I'm pretty sure it might not be testing memory memory usage correctly right now.
+        let numChars :: Int = 1024 * 1024 * 1024
             largeArray = S.take numChars $ S.repeat "A"
-        l <- S.fold_ (\acc _el -> acc + 1) (0 :: Integer) id $ toJSONStream $ VeryLargeObj largeArray largeArray 
+        l <- S.fold_ (\acc _el -> acc + 1) (0 :: Integer) id $ toJSONStream $ VeryLargeObj @IO largeArray largeArray 
         l `shouldSatisfy` (> (fromIntegral numChars * 2))
