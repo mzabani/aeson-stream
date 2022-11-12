e27cbc3bdeb453a743a3ce98f2442fa5037f359c
Marcelo Zabani <mzabani@gmail.com>
2022-11-12 16:50:19 -0300

    Add object field lazy evaluation test

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-This is just a toy Haskell library to encode JSON objects in streaming fashion. It provides a `ToJSONStream` class that can be _anyclass_ derived for types which have a `Generic` instance.
+This is just a toy Haskell library to encode large JSON objects in streaming fashion. It provides a `ToJSONStream` class that can be _anyclass_ derived for types which have a `Generic` instance. As in the example below:
+
+```haskell
+data VeryLargeObj m = VeryLargeObj {
+    field1 :: Stream (Of Text) m (),
+    field2 :: Stream (Of Text) m ()
+    }
+    deriving stock Generic
+    deriving anyclass (ToJSONStream m)
+```
+
+Make sure to have the appropriate GHC extensions enabled.
 
 It also provides a `UseToJSONInstance` class for those with existing bespoke `ToJSON` instances for their types.
 Suppose you have very intricate `ToJSON` instance for a type and you want it to derive a `ToJSONStream` instance for it without writing it by hand again, then you can just:
 
 
 ```haskell
-deriving via (UseToJSONInstance MyType) instance (ToJSONStream MyType)
+deriving via (UseToJSONInstance MyType) instance (Monad m => ToJSONStream m MyType)
 ```
 
 Naturally, this will use the `ToJSON` instance underneath so it won't stream encode your type when serializing.
--- test/MemoryUsageSpec.hs
+++ test/MemoryUsageSpec.hs
@@ -1,13 +1,24 @@
 module MemoryUsageSpec where
 
-import AesonStream (ToJSONStream(..))
+import AesonStream (ToJSONStream(..), UseToJSONInstance(..))
+import Control.DeepSeq (force, NFData)
+import Data.Aeson (ToJSON(..))
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as LBS
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import           Streaming.Prelude              ( Of
                                                 , Stream
                                                 )
 import qualified Streaming.Prelude             as S
-import Test.Hspec (Spec, it, shouldSatisfy)
+import Test.Hspec (Spec, it, shouldSatisfy, shouldBe, xit)
+import Data.ByteString (ByteString)
+import Control.Exception (onException, evaluate, SomeException (SomeException), try)
+import Data.Maybe (mapMaybe, catMaybes)
+import Control.Exception.Base (catch)
+import Debug.Trace
+import Control.Concurrent (newMVar, modifyMVar_, readMVar)
+import Control.Monad (void)
 
 
 data VeryLargeObj m = VeryLargeObj {
@@ -17,9 +28,28 @@ data VeryLargeObj m = VeryLargeObj {
     deriving stock Generic
     deriving anyclass (ToJSONStream m)
 
+data CustomJsonInstance = CustomJsonInstance {
+    c1 :: Maybe Bool,
+    c2 :: Int,
+    c3 :: [Int]
+}
+
+instance ToJSON CustomJsonInstance where
+    toJSON CustomJsonInstance{..} = toJSON (c1, c2, c3)
+
+deriving via (UseToJSONInstance CustomJsonInstance) instance (Monad m => ToJSONStream m CustomJsonInstance)
+
+data SillyObject = SillyObject {
+    s1 :: Bool,
+    s2 :: Int,
+    s3 :: [Int]
+}
+    deriving stock (Generic)
+    deriving anyclass (NFData, ToJSON, ToJSONStream m)
+
 spec :: Spec
 spec = do
-    it "Stream more than fits in memory" $ do
+    xit "Stream more than fits in memory" $ do
         -- 2^30 =~ 1GB chars 'A' streamed, plus a few characters that form
         -- a JSON object `{ "field1": "AAA..", "field2:" "AAA..." }`
         -- means more than `numChars` characters streamed in total.
@@ -27,5 +57,23 @@ spec = do
         -- I'm pretty sure it might not be testing memory memory usage correctly right now.
         let numChars :: Int = 1024 * 1024 * 1024
             largeArray = S.take numChars $ S.repeat "A"
-        l <- S.fold_ (\acc _el -> acc + 1) (0 :: Integer) id $ toJSONStream $ VeryLargeObj @IO largeArray largeArray 
+        l <- S.fold_ (\acc _el -> acc + 1) (0 :: Integer) id $ toJSONStream $ VeryLargeObj @IO largeArray largeArray
         l `shouldSatisfy` (> (fromIntegral numChars * 2))
+
+    it "UseToJSONInstance properly generates ToJSONStream instances" $ do
+        let i = CustomJsonInstance (Just False) 37 [1, 2, 3, 4]
+        streamedValue <- S.fold_ @IO (<>) "" id $ toJSONStream i
+        Aeson.encode i `shouldBe` streamedValue
+
+    it "Lazily evaluate objects fields" $ do
+        let i = SillyObject False 37 (error "Error when evaluating the last field")
+        evaluatedChunks <- newMVar @LBS.ByteString ""
+        void $ try @SomeException $ S.foldM_ @IO (\() bs -> do
+                bss <- evaluate $ force bs
+                modifyMVar_ evaluatedChunks (pure . (<> bss))
+                ) (pure ()) pure $ toJSONStream i
+        
+        evaluatedParts <- readMVar evaluatedChunks
+        fullValue <- S.fold_ @IO (<>) "" id $ toJSONStream i { s3 = [] }
+        -- Notice we just need to close the array and the object with the partially evaluated bits
+        evaluatedParts <> "]}" `shouldBe` fullValue
